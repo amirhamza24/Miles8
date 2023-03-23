8bbcf9ac5055abba8784453a47b2f1b1c42c0a4a
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-03-24 01:26:07 +0600

    Add files via upload

---
+++ Mod-48/LoadCountriesDifferentFile/src/components/Country/Country.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Country.css'
+
+const Country = (props) => {
+    console.log(props);
+    return (
+        <div className='country'>
+            <h2>Country Name: {props.name}</h2>
+            <p>Population: {props.population}</p>
+            <p>Region: {props.region}</p>
+            <p><small>Area: {props.area}</small></p>
+        </div>
+    );
+};
+
+export default Country;
