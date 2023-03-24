72214a0addd570862e6ea8d16f6c4ecb0e124825
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-03-25 01:42:38 +0600

    Add files via upload

---
+++ Conceptual1/src/components/NestedComponents/Message.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Message = () => {
+    return (
+        <div>
+            <h1>Masud valo hoye jao..</h1>
+        </div>
+    );
+};
+
+export default Message;
---
+++ Conceptual1/src/components/NestedComponents/Person.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Person = () => {
+    return (
+        <div>
+            <h2>Obaidul Kader Saheb.....</h2>
+        </div>
+    );
+};
+
+export default Person;
