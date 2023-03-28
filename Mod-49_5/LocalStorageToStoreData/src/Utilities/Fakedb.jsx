51fae93f5e3401941ee03185e1a3e213dc97b6dc
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-03-29 01:34:19 +0600

    Update Fakedb.jsx

--- Mod-49_5/LocalStorageToStoreData/src/Utilities/Fakedb.jsx
+++ Mod-49_5/LocalStorageToStoreData/src/Utilities/Fakedb.jsx
@@ -1,16 +1,42 @@
-// use localStorage to manage cart data
-const addToDb = id => {
-    const quantity = localStorage.getItem(id);
-    if(quantity) {
-        console.log('Already Exists');
-        const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity);
-    }
-    else {
-        console.log('New Item');
-        localStorage.setItem(id, 1);
+// use local storage to manage cart data
+const addToDb = id =>{
+    let shoppingCart = {};
 
+    //get the shopping cart from local storage
+    const storedCart = localStorage.getItem('shopping-cart');
+    if(storedCart){
+        shoppingCart = JSON.parse(storedCart);
+    }
+
+    // add quantity
+    const quantity = shoppingCart[id];
+    if(quantity){
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity;
+    }
+    else{
+        shoppingCart[id] = 1;
+    }
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+}
+
+const removeFromDb = id =>{
+    const storedCart = localStorage.getItem('shopping-cart');
+    if(storedCart){
+        const shoppingCart = JSON.parse(storedCart);
+        if(id in shoppingCart){
+            delete shoppingCart[id];
+            localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+        }
     }
 }
 
-export {addToDb}
+const deleteShoppingCart = () =>{
+    localStorage.removeItem('shopping-cart');
+}
+
+export {
+    addToDb, 
+    removeFromDb,
+    deleteShoppingCart
+}
