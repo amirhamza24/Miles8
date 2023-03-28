fce1e35238f6028db15fbc99490c68b80014f15b
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-03-29 01:35:50 +0600

    Update Cosmetic.jsx

--- Mod-49_5/LocalStorageToStoreData/src/Components/Cosmetics/Cosmetic/Cosmetic.jsx
+++ Mod-49_5/LocalStorageToStoreData/src/Components/Cosmetics/Cosmetic/Cosmetic.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import { addToDb } from '../../../Utilities/Fakedb';
-import './Cosmetic.css'
+import { addToDb, removeFromDb } from '../../utilities/fakedb';
+import './Cosmetic.css';
 
 const Cosmetic = (props) => {
-    const { name, price, id } = props.cosmetic;
+    const {name, price, id} = props.cosmetic;
 
-    const addToCart = (id) => {
+    const addToCart = (id) =>{
         addToDb(id);
     }
 
+    const removeFromCart = id =>{
+        removeFromDb(id);
+    }
 
     return (
         <div className='product'>
-            <h2>Buy this: {name}</h2>
-            <p>Only for: $ {price}</p>
-            <p><small>It has Id: {id}</small></p>
-            
-            <button onClick={() => addToCart(id)}>Add to Cart</button>   
+            <h2>Buy this:{name}</h2>
+            <p>Only for: ${price}</p>
+            <p><small>it has id: {id}</small></p>
+            <button onClick={() => addToCart(id)}>Add to Cart</button>
+            <button onClick={() => removeFromCart(id)}>Remove</button>
         </div>
     );
 };
-
-// beshi use korbona eta
 
 export default Cosmetic;
